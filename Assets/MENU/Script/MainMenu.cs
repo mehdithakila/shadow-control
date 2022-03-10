ea553b966a3c55646e03b3052e0a10ab5111e2dc
mehdithakila <101137160+mehdithakila@users.noreply.github.com>
2022-03-10 02:19:47 +0100

    ia_multi_solo
    t'as intéret à fonctionner zbi

---
+++ Assets/MENU/Script/MainMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingWindow;
+    public GameObject settingSolo;
+    public GameObject settingOnline;
+
+    public string Level;
+    public string Level2;
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(Level);
+    }
+    public void StartSolo()
+    {
+        SceneManager.LoadScene(Level2);
+    }
+    
+    public void SettingsButton()
+    {
+       settingWindow.SetActive(true);
+    }
+    public void OnlineButton()
+    {
+        settingOnline.SetActive(true);
+    }
+
+    public void SoloButton()
+    {
+        settingSolo.SetActive(true);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        settingWindow.SetActive(false);
+    }
+    public void CloseSoloWindow()
+    {
+        settingSolo.SetActive(false);
+    }
+    public void CloseOnlineWindow()
+    {
+        settingOnline.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
